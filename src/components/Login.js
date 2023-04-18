3c03e05b3485abb06a513493ff0285cd4501341a
Nguyen Vy <nttvy27@gmail.com>
2023-04-18 14:23:53 +0700

    Add Signup Button

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ const Login = (props) => {
         <Content>
           <CTA>
             <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+            <SignUp>GET ALL THERE</SignUp>
           </CTA>
           <BgImage />
         </Content>
@@ -72,6 +73,23 @@ const CTALogoOne = styled.img`
   min-height: 1px;
   display: block;
   width: 100%;
+`;
+
+const SignUp = styled.a`
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #0483ee;
+  }
 `;
 
 export default Login;
