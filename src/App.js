2c58cb729121182d74531245eba4665f3844098f
Nguyen Vy <nttvy27@gmail.com>
2023-04-18 09:14:09 +0700

    Create Login Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the Disney clone</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
