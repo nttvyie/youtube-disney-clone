0616b10bab1053958dfa1d2f19f0eb2c25bdddef
Nguyen Vy <nttvy27@gmail.com>
2023-04-20 21:15:10 +0700

    Add Sign Out Functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ import {
   selectUserPhoto,
   selectUserName,
   setUserLoginDetails,
+  setSignOutState,
 } from "../features/user/userSlice";
 
 const Header = (props) => {
@@ -20,20 +21,34 @@ const Header = (props) => {
     auth.onAuthStateChanged(async (user) => {
       if (user) {
         setUser(user);
-        history.push("/home");
+        history("/");
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userName]);
 
   const handleAuth = () => {
-    auth
-      .signInWithPopup(provider)
-      .then((result) => {
-        console.log(result);
-        setUser(result.user);
-      })
-      .catch(() => {});
+    if (!userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          console.log(result);
+          setUser(result.user);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setSignOutState());
+          history("/");
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    }
   };
 
   const setUser = (user) => {
@@ -49,7 +64,9 @@ const Header = (props) => {
   return (
     <Nav>
       <Logo>
-        <img src="/images/logo.svg" alt="Disney+" />
+        <a href="/">
+          <img src="/images/logo.svg" alt="Disney+" />
+        </a>
       </Logo>
 
       {!userName ? (
@@ -82,7 +99,12 @@ const Header = (props) => {
               <span>SERIES</span>
             </a>
           </NavMenu>
-          <UserImg src={userPhoto} alt={userName} />
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign Out</span>
+            </DropDown>
+          </SignOut>
         </>
       )}
     </Nav>
@@ -200,8 +222,45 @@ const Login = styled.a`
 `;
 
 const UserImg = styled.img`
-  height: 50px;
-  border-radius: 50%;
+  height: 100%;
+`;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background-color: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 0px;
+  padding: 10px;
+  font-size: 13px;
+  letter-spacing: 3px;
+  width: 100px;
+  display: none;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    ${DropDown} {
+      display: block;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 export default Header;
