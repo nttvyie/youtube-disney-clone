c12fd51880e76dddd234a8295cf849b601388db8
Nguyen Vy <nttvy27@gmail.com>
2023-04-20 10:32:13 +0700

    Setup Firebase & Add Sign In with Google Popup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
 
 const Header = (props) => {
+  const handleAuth = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch(() => {});
+  };
+
   return (
     <Nav>
       <Logo>
         <img src="/images/logo.svg" alt="Disney+" />
       </Logo>
+
       <NavMenu>
         <a href="/">
           <img src="/images/home-icon.svg" alt="HOME" />
@@ -32,7 +43,8 @@ const Header = (props) => {
           <span>SERIES</span>
         </a>
       </NavMenu>
-      <Login>Login</Login>
+
+      <Login onClick={handleAuth}>Login</Login>
     </Nav>
   );
 };
@@ -143,6 +155,7 @@ const Login = styled.a`
     background-color: #f9f9f9;
     color: #000;
     border-color: transparent;
+    cursor: pointer;
   }
 `;
 
